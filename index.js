dd96a9b3319f904223ac01c5465b373c60707b75
wzbg <wzbg@github.com>
2015-09-11 11:05:11 +0800

    支持冒号分割的URL

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ var urlParse = require('url').parse;
 var isImage = require('is-image');
 
 module.exports = function (url) {
-	return url ? isImage(urlParse(url).pathname.split(':')[0]) : false;
+  if (!url) return false;
+  var pathname = urlParse(url).pathname;
+  if (!pathname) return false;
+  return isImage(pathname.split(':')[0]);
 };
