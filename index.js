6209ead64d413051549d6de729c4e027dd82e5d9
wzbg <wzbg@github.com>
2015-09-19 20:52:26 +0800

    支持2个HTTP的URL

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ module.exports = function (url) {
   if (!url) return false;
   var pathname = urlParse(url).pathname;
   if (!pathname) return false;
-  return isImage(pathname.split(':')[0]);
+  var imagename = pathname.substring(pathname.lastIndexOf('/') + 1);
+  if (!imagename) return false;
+  return isImage(imagename.split(':')[0]);
 };
