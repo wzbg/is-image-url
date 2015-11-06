77600ac676445f9908896fb04d8530bab4d3a399
yecheng <yecheng@amandapp.com>
2015-11-06 16:38:17 +0800

    支持切分URL

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ var isUrl = require('is-url');
 module.exports = function (url, accurate) {
   if (!url) return false;
   if (!isUrl(url)) return isImage(url);
+  var http = url.lastIndexOf('http');
+  if (http == -1) return false;
+  url = url.substring(http);
   var pathname = urlParse(url).pathname;
   if (!pathname) return false;
-  if (isImage(pathname.split(':')[0])) {
-    return true;
-  }
+  var last = pathname.search(/[:?&]/);
+  if (last != -1)
+    pathname = pathname.substring(0, last);
+  if (isImage(pathname)) return true;
   try {
     if (!accurate) return false;
     var res = request('GET', url);
